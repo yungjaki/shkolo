a2f8d8a4bd302ee02148cedc466d87ae27abb004
Димитър Димитров <88945970+yungjaki@users.noreply.github.com>
2026-02-18 09:12:35 +0200

    Update GradeBubble.tsx

--- src/components/GradeBubble.tsx
+++ src/components/GradeBubble.tsx
@@ -3,9 +3,10 @@ import { gradeColor } from "../utils/gradeColor";
 export default function GradeBubble({ value }: { value: number }) {
   return (
     <div
-      className={`w-[34px] h-[34px] rounded-full flex items-center justify-center text-white text-[14px] font-semibold ${gradeColor(
-        value
-      )}`}
+      className={`w-[32px] h-[32px] rounded-full
+      flex items-center justify-center
+      text-white text-[14px] font-semibold leading-none
+      ${gradeColor(value)}`}
     >
       {value}
     </div>
