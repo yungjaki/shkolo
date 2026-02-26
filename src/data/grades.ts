10c16b2c2df139041a3bb52e3675241ea2302cca
Димитър Димитров <88945970+yungjaki@users.noreply.github.com>
2026-02-26 12:01:07 +0200

    Update grades.ts

--- src/data/grades.ts
+++ src/data/grades.ts
@@ -57,7 +57,7 @@ export const gradesData: SubjectGrades[] = [
       },
             {
         term: "Втори срок",
-        current: [5].map(g),
+        current: [6].map(g),
         termGrade: 0,
       },
     ],
